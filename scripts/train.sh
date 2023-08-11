368e5de3cc4d237b761d852d045173a764e3eb55
Said Togru <said.togru@gmail.com>
2023-08-11 03:35:44 +0200

    add new parameter for optuna & increase trials

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,17 @@
 # Schedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-python src/train.py 
+python src/train.py model=coatnet
+
+python src/train.py model=densenet
+
+python src/train.py model=efficientnetv2
+
+python src/train.py model=mobilevit
+
+python src/train.py model=resnet50
+
+python src/train.py model=swintransformer
 
 
 

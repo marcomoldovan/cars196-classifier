4d099e792f164ee68199148eda6778d892e30e7e
Said Togru <said.togru@gmail.com>
2023-08-11 23:45:17 +0200

    adjust epochs and trials for small test

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Schedule execution of all models
 
-python src/train.py model=coatnet
+python src/train.py model=coatnet model=coatnet
 
-python src/train.py model=densenet
+python src/train.py model=densenet task_name=densenet
 
-python src/train.py model=efficientnetv2
+python src/train.py model=efficientnetv2 task_name=efficientnetv2
 
-python src/train.py model=mobilevit
+python src/train.py model=mobilevit task_name=mobilevit
 
-python src/train.py model=resnet50
+python src/train.py model=resnet50 task_name=resnet50
 
-python src/train.py model=swintransformer
+python src/train.py model=swintransformer task_name=swintransformer
 
 
 

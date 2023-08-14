5b815cbc086d0d93585e47d31e974a8c4beb4bbb
Said Togru <said.togru@gmail.com>
2023-08-14 08:14:52 +0200

    adjust naming in batch job

--- scripts/semifinal_efficientnetv2.sh
+++ scripts/semifinal_efficientnetv2.sh
@@ -2,14 +2,14 @@
 
 #SBATCH --job-name=efficientnetv2_training
 #SBATCH --partition=Nvidia2060          
-#SBATCH --output=logs/final_efficientnetv2.%j.out
-#SBATCH --error=logs/final_efficientnetv2.%j.err
+#SBATCH --output=logs/semifinal_efficientnetv2.%j.out
+#SBATCH --error=logs/semifinal_efficientnetv2.%j.err
 
 # Activate the virtual environment
 source cars196-classifier/.venv/bin/activate
 
 # Execute the model training
-python cars196-classifier/src/train.py model="efficientnetv2" task_name="final_efficientnetv2" data.batch_size=32 trainer.max_epochs=100
+python cars196-classifier/src/train.py model="efficientnetv2" task_name="semifinal_efficientnetv2" data.batch_size=32 trainer.max_epochs=10
 
 # Deactivate the virtual environment
 deactivate
